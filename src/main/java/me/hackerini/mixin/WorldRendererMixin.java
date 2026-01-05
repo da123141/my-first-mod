4254d44c2f7756954f8a4574a6b77f6403a8f667
da123141 <110171811+da123141@users.noreply.github.com>
2026-01-05 13:35:01 +0100

    mod7

--- src/main/java/me/hackerini/mixin/WorldRendererMixin.java
+++ src/main/java/me/hackerini/mixin/WorldRendererMixin.java
@@ -3,7 +3,10 @@ package me.hackerini.mixin;
 import me.hackerini.modules.Esp;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,9 +17,18 @@ import org.joml.Matrix4f;
 public class WorldRendererMixin {
     
     @Inject(at = @At("RETURN"), method = "render")
-    private void onRender(RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo info) {
-        // Wywołujemy nasze ESP na samym końcu renderowania świata
-        // MatrixStack matrices = new MatrixStack(); // W nowszych wersjach trzeba uważać na stos macierzy
-        Esp.onRender(new MatrixStack());
+    private void onRender(
+        RenderTickCounter tickCounter, 
+        boolean renderBlockOutline, 
+        Camera camera, 
+        GameRenderer gameRenderer, 
+        LightmapTextureManager lightmapTextureManager, 
+        Matrix4f matrix4f, 
+        Matrix4f matrix4f2, 
+        CallbackInfo info
+    ) {
+        // Tworzymy MatrixStack i wywołujemy ESP
+        MatrixStack matrices = new MatrixStack();
+        Esp.onRender(matrices);
     }
 }
